599220a54a1cda6035ca372d58757d8ca463ce26
panda001945 <bkomen638@gmail.com>
2022-09-08 16:22:05 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+
+
+
 /**
-*main - print the string in the put function
+
+* main - print the string in the put function
+
 *
-*Description: using the main function
-*this program prints "programming is like building a multilingual puzzle"
-*Return: 0
+
+* Description: using the main function
+
+* this program prints "Programming is like building a multilingual puzzle
+
+* Return: 0
+
 */
+
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
 }
