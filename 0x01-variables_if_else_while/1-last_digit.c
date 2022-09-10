f29ab51b05ce9c7d40ec83f6e9c8fec127172a94
panda001945 <bkomen638@gmail.com>
2022-09-10 01:23:09 -0700

    teddy

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,17 +6,11 @@
 
 /**
 * main - print if the number is postive, zero, or negative
-
 * Description: using the main function
-
 * this program prints "Programming is positive, zero, or negative
-
 * Return: 0
-
 */
-
 int main(void)
-
 {
 int n;
 
