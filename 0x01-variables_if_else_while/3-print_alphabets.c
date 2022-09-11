6a2c43fddabc3487b758a8cd080c51b3dfa67362
panda001945 <bkomen638@gmail.com>
2022-09-11 03:36:24 -0700

    teddy

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,10 @@
  */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
+         char ch;
+
+         for (ch = 'a' ; ch <= 'z' ; ch++)
+}
         putchar(ch);
 }
 for (ch = 'A' ; ch <= 'Z" ; ch++)
